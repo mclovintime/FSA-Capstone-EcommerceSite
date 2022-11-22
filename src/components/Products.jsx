526177665601baab1a691f7d0c25acfe088d8e38
Thomas McLaughlin <123tommygun1@gmail.com>
2022-11-22 14:13:53 -0500

    fix

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,36 @@
 import { getProducts } from "../api-adapter";
 import React, {useState, useEffect} from "react";
+// import submit login and the register login
 
 const Products = () =>   {
 
+    async function submitLogin()    {
+        // 
+    }
+
+    const [username, setUsername] = useEffect("")
+    const [password, setPassword] = useEffect("")
 
     return(
 <div>
     <h1>hello testing</h1>
-    <button onClick={() =>  getProducts()}>test get all products check console</button>
+   
+
+        <h3>Username:</h3>
+    <input
+          type="text"
+          required
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+          <h3>Password:</h3>
+    <input
+          type="text"
+          required
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button onClick={() => submitLogin()}>Login</button>
 </div>
     )
 }
