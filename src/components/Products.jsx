7f5ae7f05c6cf293fc9fe9ad3b48b9d67afaa9dd
Thomas McLaughlin <123tommygun1@gmail.com>
2022-11-22 14:17:19 -0500

    fix part 2: electric boogaloo

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,36 +1,14 @@
-import { getProducts } from "../api-adapter";
+
 import React, {useState, useEffect} from "react";
-// import submit login and the register login
+import { getProducts } from "../api-adapter";
+
 
 const Products = () =>   {
 
-    async function submitLogin()    {
-        // 
-    }
-
-    const [username, setUsername] = useEffect("")
-    const [password, setPassword] = useEffect("")
 
     return(
 <div>
-    <h1>hello testing</h1>
-   
 
-        <h3>Username:</h3>
-    <input
-          type="text"
-          required
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-          <h3>Password:</h3>
-    <input
-          type="text"
-          required
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button onClick={() => submitLogin()}>Login</button>
 </div>
     )
 }
