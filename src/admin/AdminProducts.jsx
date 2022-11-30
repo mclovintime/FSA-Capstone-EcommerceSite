d3bc355ec64a9f93e389cf332249db349c5c04bf
Maaya <yamauchimaaya@gmail.com>
2022-11-30 09:08:15 -0600

    delete button now working and getting allusers for admin

--- src/admin/AdminProducts.jsx
+++ src/admin/AdminProducts.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { getProducts } from "../api-adapter";
+import { getProducts, deleteProduct } from "../api-adapter";
 
 import { Link, useNavigate, useParams } from "react-router-dom";
 import CreateProduct from "./CreateProduct";
@@ -28,11 +28,25 @@ const AdminProducts = () => {
     e.preventDefault()
     navigate("/mycart/cart_items")
   }
+
+  async function handleDelete(productId) {
+    console.log(productId, "DELETE PRODUCTID");
+    const adminProductId = Number(productId);
+    const token = localStorage.getItem("token")
+    const deleted = await deleteProduct(adminProductId, token);
+    console.log(deleted, "here is deleted");
+    // if (deleted.success) {
+    //   navigate("/mycart/cart_items");
+    // }
+  }
   
   return (
     <div>
       <h2>All Products</h2>
       <CreateProduct/>
+
+      
+
       <div id="container">
         {products.length ? (
           products.map((product) => {
@@ -58,8 +72,12 @@ const AdminProducts = () => {
               
 
               <EditProduct product={product}/>
-              <DeleteProducts products={products}/>
-
+              <button
+              id={product.id ? `${product.id}` : null}
+               onClick={() => handleDelete(product.id)}>
+                
+                Delete Product
+              </button>
               </div>
             );
           })
