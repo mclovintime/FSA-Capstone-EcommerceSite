6add9e62c0c5def7ad16291dcfb18b05a890e677
lucasawest <lucasawest1@gmail.com>
2022-12-01 20:42:45 -0700

    added beginning of stripe

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,15 +1,41 @@
-import react from "react";
+import React from 'react'
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
 
-const Checkout = () => {
+import STRIPE_PUBLISHABLE from '../constants/Stripe';
+import PAYMENT_SERVER_URL from '../constants/Server';
 
+const CURRENCY = 'USD';
 
-    return (
-        <div>
-        <div>Checkout</div>
-        <div>Checkout</div>
-        <div>Checkout</div>
-        </div>
-    )
-}
+const fromDollarToCent = amount => amount * 100;
 
-export default Checkout
+const successPayment = data => {
+  alert('Payment Successful');
+};
+
+const errorPayment = data => {
+  alert('Payment Error');
+};
+
+const onToken = (amount, description) => token =>
+  axios.post(PAYMENT_SERVER_URL,
+    {
+      description,
+      source: token.id,
+      currency: CURRENCY,
+      amount: fromDollarToCent(amount)
+    })
+    .then(successPayment)
+    .catch(errorPayment);
+
+const Checkout = ({ name, description, amount }) =>
+  <StripeCheckout
+    name={name}
+    description={description}
+    amount={fromDollarToCent(amount)}
+    token={onToken(amount, description)}
+    currency={CURRENCY}
+    stripeKey={STRIPE_PUBLISHABLE}
+  />
+
+export default Checkout;
---
+++ src/constants/Server.jsx
@@ -0,0 +1,3 @@
+const PAYMENT_SERVER_URL = process.env.NODE_ENV === 'http://localhost:8080';
+
+export default PAYMENT_SERVER_URL;
