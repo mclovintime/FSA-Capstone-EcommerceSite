dafed0035c2547d26f4d510f9869d5e22cb9d7b5
Maaya <yamauchimaaya@gmail.com>
2022-12-05 09:16:09 -0600

    fixed login button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,44 +3,60 @@ import { Outlet, Link } from "react-router-dom";
 import { LoggedIn, Logout } from "./";
 import "./Navbar.css";
 
-
 const Navbar = (props) => {
   const user = props.user;
-  
-  const setUser = props.setUser
-  const handleLogout = props.handleLogout
-  const isLoggedIn = props.isLoggedIn
-  const setIsLoggedIn = props.setIsLoggedIn
+
+  const setUser = props.setUser;
+  const handleLogout = props.handleLogout;
+  const isLoggedIn = props.isLoggedIn;
+  const setIsLoggedIn = props.setIsLoggedIn;
   return (
     <div>
-      <div>
-        
-
-      
-      </div>
+      <div></div>
       <div id="navbar">
-      <Link to="/" id="logo">DimTech</Link>
-      <LoggedIn user={user}/>
-       
-        <Link id="productsThing" to="/Products">Products</Link>
-        <Link to="/Login"><i className="fa-solid fa-user"></i></Link>
+        <Link to="/" id="logo">
+          DimTech
+        </Link>
+        <LoggedIn user={user} />
+
+        <Link id="productsThing" to="/Products">
+          Products
+        </Link>
+        <Link to="/Login">
+          <i className="fa-solid fa-user"></i>
+        </Link>
         {/* <Link to="/Register">Register</Link> */}
         <Link to="/mycart/cart_items"></Link>
-        
-        {user ? <Link to="/mycart/cart_items"><i className="fa-solid fa-cart-shopping"></i></Link>  :
-        <Link to="/guestcart"><i id="cartIcon" className="fa-solid fa-cart-shopping"></i></Link>
-        }
 
-        {user ? <Link to="/MyProfile"><i className="fa-solid fa-address-card"></i></Link>  : 
-                <Link to="/Home"><i className="fa-solid fa-address-card"></i></Link>
+        {user ? (
+          <Link to="/mycart/cart_items">
+            <i className="fa-solid fa-cart-shopping"></i>
+          </Link>
+        ) : (
+          <Link to="/guestcart">
+            <i id="cartIcon" className="fa-solid fa-cart-shopping"></i>
+          </Link>
+        )}
 
-        }
+        {user ? (
+          <Link to="/MyProfile">
+            <i className="fa-solid fa-address-card"></i>
+          </Link>
+        ) : (
+          <Link to="/Home">
+            <i className="fa-solid fa-address-card"></i>
+          </Link>
+        )}
 
-        {user && user.is_admin === true ? (
-          <Link to="/Admin">Admin</Link>
-        ): null}
-        
-        {isLoggedIn ? <Logout handleLogout={handleLogout} setUser={setUser} setIsLoggedIn={setIsLoggedIn}/> : null}
+        {user && user.is_admin === true ? <Link to="/Admin">Admin</Link> : null}
+
+        {isLoggedIn ? (
+          <Logout
+            handleLogout={handleLogout}
+            setUser={setUser}
+            setIsLoggedIn={setIsLoggedIn}
+          />
+        ) : null}
       </div>
       <Outlet></Outlet>
     </div>
