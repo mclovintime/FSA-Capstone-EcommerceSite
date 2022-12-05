0ac3ccf134c4c29b5b2363c07c82a30436476655
lucasawest <lucasawest1@gmail.com>
2022-12-04 18:39:59 -0700

    css touch ups and stripe progress

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -124,7 +124,7 @@ const Main = () => {
                 />
               }
             />
-            <Route path="/checkout" element={<Checkout />} />
+            {/* <Route path="/checkout" element={<Checkout />} /> */}
             <Route path="/contactform" element={<ContactForm />} />
             <Route path="/Admin" element={<AdminPage user={user} />} />
             <Route path="/AdminUsers" element={<AdminUsers user={user} />} />
