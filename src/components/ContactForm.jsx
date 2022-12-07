a16b22cbbdb7919823c83731e52805a7e56abea4
Maaya <yamauchimaaya@gmail.com>
2022-12-06 19:39:26 -0600

    added media query and cleaned up code

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,31 +1,27 @@
 import "./contactForm.css";
 
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
- const ContactForm = () => {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+const ContactForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-    async function handleSubmit(e) {
-        e.preventDefault();
-        alert(
-            "Thank you for reaching out! We'll be in touch shortly."
-        );
-        setFirstName("");
-        setLastName("");
-        setEmail("");
-        setMessage("");
-    }
-
+  async function handleSubmit(e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! We'll be in touch shortly.");
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setMessage("");
+  }
 
   return (
- 
     <div className="contact-form">
       <h2 className="cf-header">We would love to hear from you!</h2>
-    <form onSubmit={handleSubmit} className="contact-input">
-    <input 
+      <form onSubmit={handleSubmit} className="contact-input">
+        <input
           type="text"
           name="first-name"
           value={firstName}
@@ -34,7 +30,7 @@ import React, {useState} from 'react'
           placeholder="First name"
           className="contactFirstName"
         />
-    <input
+        <input
           type="text"
           name="last-name"
           value={lastName}
@@ -42,9 +38,8 @@ import React, {useState} from 'react'
           label="Last Name"
           placeholder="Last name"
           className="contactLastName"
-
         />
-    <input
+        <input
           type="text"
           name="email"
           value={email}
@@ -52,9 +47,8 @@ import React, {useState} from 'react'
           label="Email"
           placeholder="Email"
           className="contactEmail"
-
         />
-    <textarea
+        <textarea
           type="text"
           name="message"
           value={message}
@@ -62,16 +56,13 @@ import React, {useState} from 'react'
           label="Message"
           placeholder="Message"
           className="contactMessage"
-
         />
         <button className="contact-form-button" type="submit">
           Submit
         </button>
-    </form>
-    
+      </form>
     </div>
-    
-  )
-}
+  );
+};
 
-export default ContactForm
+export default ContactForm;
