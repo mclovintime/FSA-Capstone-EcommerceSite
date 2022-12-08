d598332d26edea8db4ab83ea3eda37c2a48310b4
Maaya <yamauchimaaya@gmail.com>
2022-12-07 20:43:01 -0600

    re-arranged navbar

--- src/components/LoggedIn.jsx
+++ src/components/LoggedIn.jsx
@@ -3,12 +3,11 @@ import "./loggedIn.css";
 import "./Navbar.css";
 
 const LoggedIn = (props) => {
-  console.log(props.user)
   const user = props.user;
 
   return (
     <div>
-      <h3 id="fixingWelcome" className="leftNav"> Welcome, {user && user.username ? user.username : "Guest"} </h3>
+      <h3 id="fixingWelcome" > Welcome, {user && user.username ? user.username : "Guest"} </h3>
     </div>
   );
 };
